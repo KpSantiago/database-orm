5de1a0b6f0b83aaab227cb4949b6795407c7be29
KpSantiago <santiago.kauajose11@gmail.com>
2024-09-16 10:58:25 -0300

    fix: remove error from default constraint

--- src/migrations/data-types/constraint-types.js
+++ src/migrations/data-types/constraint-types.js
@@ -1,11 +1,9 @@
-import { MySQLDataTypes } from "./MySQLDataTypes.js"
-
 export const constraint = (constraint, value) => {
     const types = {
         nullable: value ? ' NULL ' : ' NOT NULL ',
         unique: value ? ' UNIQUE ' : '',
         primaryKey: value ? ' PRIMARY KEY ' : '',
-        default: MySQLDataTypes[value] ? ' DEFAULT ' + MySQLDataTypes[value] : ' DEFAULT ' + value + ' ',
+        default: value ? ' DEFAULT ' + value + ' ' : '',
         autoIncrement: value ? ' AUTO_INCREMENT ' : '',
         after: value ? ' AFTER ' + value : '',
     }
