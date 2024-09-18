f29d9ce1d6b9cbec9362dafd4149d4550105429f
KpSantiago <santiago.kauajose11@gmail.com>
2024-09-18 20:28:15 -0300

    fix: update package name

--- src/migrations/config.ts
+++ src/migrations/config.ts
@@ -5,6 +5,6 @@ fs.mkdirSync("databaseORM");
 fs.mkdirSync("databaseORM/migrations");
 
 fs.writeFileSync(`${path.dirname("")}/databaseORM/connection.ts`, 
-`import { Connection } from "@kpsantiago/databaseORM";
+`import { Connection } from "@kpsantiago/database-orm";
 
 export const connection = new Connection("url");`);
