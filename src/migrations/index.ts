787b2a61358c26c00b277b20bd8d4c116d9475b6
KpSantiago <santiago.kauajose11@gmail.com>
2024-09-18 21:24:57 -0300

    feat: add run migrates command to script file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
-
+import { execSync } from "node:child_process";
+import path from "node:path"
 const args = process.argv.slice(2);
 
 if (args[0] == "--init") {
     require("./config.js");
-} else if (args[0] == "--init") {
+} else if (args[0] == "--migrate-create") {
     require("./create.js");
+} else if (args[0] == "--migrate") {
+    execSync(`tsnd --transpile-only ${path.dirname("")}/databaseORM/migrations/migrations.ts`)
 } else {
-    console.log("Invalid command \n\n### Initialize\norm --init\n\n### Migrations\norm --migrate-create\n\n");
+    console.log("Invalid command \n\n### Initialize\norm --init\n\n### Migrations\norm --migrate-create\n\n### Run migrations\norm --migrate\n\n");
 }
